e0a7835ddb7678a1a98e4eb7701d72e90ccd0088
Jonathan Rubinger <MeaninglessJargon@gmail.com>
2013-08-06 15:40:54 -0400

    added barriers, constrained horizontal motion

--- Assets/BasicStrafe.cs
+++ Assets/BasicStrafe.cs
@@ -4,18 +4,35 @@ using System.Collections;
 public class BasicStrafe : MonoBehaviour {
 	
 	public float moveSpeed;
+	public GameObject spawnPoint;
+	public float horizontalRange;
+	float centerX;
 	
 	// Use this for initialization
 	void Start () {
-		
+		centerX = spawnPoint.transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		float hor = Input.GetAxis ("Horizontal");
-		transform.Translate(hor * moveSpeed, 0, 0);
 		
+		float newX = transform.position.x + hor * moveSpeed;
+		
+		if (newX < centerX + horizontalRange && newX > centerX - horizontalRange)
+			transform.Translate(hor * moveSpeed, 0, 0);
+		
+	}
+	
+	void FixedUpdate() {
+	
+		if (transform.position.x > centerX + horizontalRange) {
+			transform.Translate ((centerX + horizontalRange) - transform.position.x, 0, 0);
+		}
+		if (transform.position.x < centerX - horizontalRange) {
+			transform.Translate ((centerX - horizontalRange) - transform.position.x, 0, 0);
+		}
 		
 	}
 }
